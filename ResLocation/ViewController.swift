2e1ac6f2b0978a95936cfad16c5047abb979bcb6
huynhquangtoan <huynhquangtoan1997@gmail.com>
2019-05-31 09:31:01 +0700

    Initial Commit

---
+++ ResLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResLocation
+//
+//  Created by Huynh Quang Toan on 5/31/19.
+//  Copyright © 2019 Huynh Quang Toan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
